407a0fba4166f0da4190a7e241cc8b5e52f647a2
ssachdev <sanjay.sachdev@hotmail.com>
2021-06-05 17:02:07 +0530

    Finished the blogs display feature with static blog data

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,10 @@ from datetime import date
 
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
-        "image": "mountain.jpg",
+        "image": "mountains.jpg",
         "author": "Sanjay",
         "date": date(2021, 6, 4),
         "title": "Mountain Hiking",
@@ -65,10 +65,19 @@ posts = [
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=(lambda post: post['date']))
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
